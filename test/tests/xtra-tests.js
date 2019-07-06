ce9b5f2654c0678341619841d7228f0854176302
e2tha-e <ee@electric-eloquence.io>
2019-07-06 12:35:49 -0400

    test covering help task

---
+++ test/tests/xtra-tests.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const path = require('path');
+
+// Instantiate a gulp instance and assign it to the fp const.
+process.env.ROOT_DIR = path.normalize(`${__dirname}/..`);
+const fp = require('fepper/tasker');
+
+require('../../import~extend');
+
+describe('fp import:help', function () {
+  it('should print help text', function (done) {
+    fp.runSeq(
+      'import:help',
+      done
+    );
+  });
+});
